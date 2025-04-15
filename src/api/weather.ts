eb887b0a1121aa73e2037e36ff3ba4be813a11ca
zzj <zhengzijian56@gmail.com>
2025-04-15 11:22:16 +0800

    update types

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,3 +1,5 @@
+import { WeatherAPIResponse } from '@/app/types';
+
 export const fetchWeather = async (params: {city: string}) => {
   const response = await fetch(
     `https://restapi.amap.com/v3/weather/weatherInfo?city=${params.city}&key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&extensions=all`
